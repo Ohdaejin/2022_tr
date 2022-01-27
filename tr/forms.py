9a75b76f83eb6ad4ada597a299dd019635dbf2cd
ohdaejin <xla3095@gmail.com>
2022-01-27 11:29:30 +0900

    답변 폼 구현

--- tr/forms.py
+++ tr/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from tr.models import Question
+from tr.models import Question, Answer
 
 
 class QuestionForm(forms.ModelForm):
@@ -9,4 +9,12 @@ class QuestionForm(forms.ModelForm):
         labels = {
             'subject': '제목',
             'content': '내용',
+        }
+
+class AnswerForm(forms.ModelForm):
+    class Meta:
+        model = Answer
+        fields = ['content']
+        labels = {
+            'content': '답변내용',
         }
--- tr/views.py
+++ tr/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Question
-from .forms import QuestionForm
+from .forms import QuestionForm, AnswerForm
 
 def index(request):
     """
@@ -24,8 +24,18 @@ def answer_create(request, question_id):
     tr 답변등록
     """
     question = get_object_or_404(Question, pk=question_id)
-    question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
-    return redirect('tr:detail', question_id=question.id)
+    if request.method == "POST":
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.create_date = timezone.now()
+            answer.question = question
+            answer.save()
+            return redirect('tr:detail', question_id=question.id)
+    else:
+        form = AnswerForm()
+    context = {'question': question, 'form': form}
+    return render(request, 'tr/question_detail.html', context)
 
 def question_create(request):
     """
