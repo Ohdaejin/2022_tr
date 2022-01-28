ef1f8bd63dc5d6150dccf4a3f87df0be038cf06a
ohdaejin <xla3095@gmail.com>
2022-01-28 10:02:21 +0900

    그룹 메인 페이지 완성

--- group/urls.py
+++ group/urls.py
@@ -1,4 +1,8 @@
+from django.urls import path
+from . import views
+
 app_name = 'group'
 
 urlpatterns = [
+    path('', views.main, name='main'),
 ]
--- group/views.py
+++ group/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+def main(request):
+
+    return render(request, 'group/group_main.html')
